gwen-lg/vobsubocr

update to use Time struct of subtitles_utils

src/ocr.rs
@@ -12,7 +12,7 @@ use leptess::{
 };
 use rayon::prelude::*;
 use scoped_tls_hkt::scoped_thread_local;
-use subparse::timetypes::TimeSpan;
+use subtitles_utils::time::TimeSpan;
 use thiserror::Error;
 
 scoped_thread_local!(static mut TESSERACT: Option<TesseractWrapper>);

src/preprocessor.rs
@@ -8,8 +8,10 @@ use image::{GrayImage, ImageBuffer, Luma};
 use iter_fixed::IntoIteratorFixed;
 use log::warn;
 use rayon::prelude::*;
-use subparse::timetypes::{TimePoint, TimeSpan};
-use subtitles_utils::{vobsub, SubError};
+use subtitles_utils::{
+    time::{TimePoint, TimeSpan},
+    vobsub, SubError,
+};
 
 pub struct ImagePreprocessOpt {
     threshold: f32,

gwen-lg/subtile-ocr

src/ocr.rs
@@ -12,7 +12,7 @@ use leptess::{
 };
 use rayon::prelude::*;
 use scoped_tls_hkt::scoped_thread_local;
-use subparse::timetypes::TimeSpan;
+use subtile::time::TimeSpan;
 use thiserror::Error;
 
 scoped_thread_local!(static mut TESSERACT: Option<TesseractWrapper>);

src/preprocessor.rs
@@ -7,8 +7,10 @@ use image::{GrayImage, ImageBuffer, Luma};
 use iter_fixed::IntoIteratorFixed;
 use log::warn;
 use rayon::prelude::*;
-use subparse::timetypes::{TimePoint, TimeSpan};
-use subtile::{vobsub, SubError};
+use subtile::{
+    time::{TimePoint, TimeSpan},
+    vobsub, SubError,
+};
 
 pub struct ImagePreprocessOpt {
     threshold: f32,